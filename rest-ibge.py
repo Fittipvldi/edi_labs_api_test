31fb94b72af79b396e88f85a70e87307c23c57fc
Fittipvldi <henrique.gfitti@gmail.com>
2021-03-10 22:47:43 -0300

    refatoracao

--- rest-ibge.py
+++ rest-ibge.py
@@ -1,39 +1,27 @@
 import requests
 import csv
 
-response = requests.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados/')
-json = response.json()
+try:
+    response = requests.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados/')
 
-# print(json)
+    if response.status_code == 200:
+        dados = response.json()
 
-norte = 0
-nordeste = 0
-sul = 0
-sudeste = 0
-centro_oeste = 0
+        regioes = []
 
-for i in json:
-    regiao = (i['regiao']['nome'])
+        for i in dados:
+            regioes.append(i['regiao']['nome'])
 
-    if regiao == 'Norte':
-        norte += 1
-    elif regiao == 'Nordeste':
-        nordeste += 1
-    elif regiao == 'Sul':
-        sul += 1
-    elif regiao == 'Sudeste':
-        sudeste += 1
+        nome_regioes = set(regioes)
+
+        with open('qtd_regioes.csv', 'w') as file:
+            columns = ['Região', 'Quantidade'] # header
+            escrever = csv.DictWriter(file, fieldnames=columns, delimiter='|', lineterminator='\n')
+            escrever.writeheader() # escreve o header
+
+            for regiao in sorted(nome_regioes): # sorted retorna em ordem alfabética
+                escrever.writerow({'Região': regiao, 'Quantidade': regioes.count(regiao)})
     else:
-        centro_oeste += 1
-
-print(f'Norte: {norte}, Nordeste: {nordeste}, Sul: {sul}, Sudeste: {sudeste}, Centro-Oeste: {centro_oeste}')
-
-with open('qtd_regioes.csv', 'w', newline='') as file:
-    the_writer = csv.writer(file, delimiter='|')
-
-    the_writer.writerow(['Regiao', 'Qtd. Estados']) # header
-    the_writer.writerow(['Norte', norte])
-    the_writer.writerow(['Nordeste', nordeste])
-    the_writer.writerow(['Sul', sul])
-    the_writer.writerow(['Sudeste', sudeste])
-    the_writer.writerow(['Centro-Oeste', centro_oeste])
+        print(f'ERRO! {response.status_code}')
+except Exception as erro:
+    print(f"ERRO! {erro}")
